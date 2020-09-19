cb16b93ed2afa98c4ca8b5e9b5983d2f0c3e5613
ipmach <andre.gg96@gmail.com>
2020-09-19 05:34:51 +0200

    First flak draft #5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Mongo_class import Mongo_pizza
 from Eaternity_class import Eaternity_class
-
+from migros_class import MigrosAPI
+from tqdm import tqdm
 
 class Logic():
 
@@ -8,6 +9,7 @@ class Logic():
         print("Loading...")
         self.pm = Mongo_pizza()
         self.ec = Eaternity_class()
+        self.ma = MigrosAPI()
         self.user = "Peter"
         self.kitchen = "Kitchen_Peter"
         print("Server ready")
@@ -36,5 +38,12 @@ class Logic():
         aux = [self.pm.get_ingredient(i) for i in ingredients]
         aux = [self.conver_ingredient(i,[]) for i in aux]
         pizza_name = self.pm.classify_pizza(ingredients)
-        return self.ec.put_recipe(pizza_name, self.kitchen,
-                                  pizza_name, aux, location)
+        aux = self.ec.put_recipe(pizza_name, self.kitchen,
+                                 pizza_name, aux, location)
+        for i in tqdm(ingredients):
+            try:
+                aux[i + " Nutritional"] = self.ma.get_nutritients(i)
+            except IndexError:
+                aux[i + " Nutritional"] = "None"
+
+        return aux
