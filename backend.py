a252abca64f2c3db6b4572aa8fdc253c6b4e4825
ipmach <andre.gg96@gmail.com>
2020-09-19 13:42:49 +0200

    backend.py #5

---
+++ backend.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from main import Logic
+from flask import request
+from markupsafe import escape
+
+
+app = Flask(__name__)
+l = Logic()
+
+@app.route('/alive')
+def isAlive():
+    return {"State": "200 server alive"}
+
+@app.route('/predict/')
+def prediction():
+    return l.return_data(["Pineapple", "Pepperoni"])
